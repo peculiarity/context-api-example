0f1d80e55e054f704e16ef241940af35eacb283e
Pex <georgi.h.georgieff@gmail.com>
2018-04-12 12:26:53 +0200

    Single Consumer , wrapped into a single Provider.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 const {Provider, Consumer} = React.createContext("Hello World from BEYOND!");
 
+const App = () => (
+  <Provider value="Hello Georgi">
+    <Consumer>
+      {
+        value => <h1> {value} </h1>
+      }
+    </Consumer>
+  </Provider>
+)
 
-export default () => <Consumer>
-  {
-    value => <h1> {value} </h1>
-  }
-</Consumer>;
+
+export default App;
